48089d54353b2c504982af37e42bdf0fb3b6dc71
Carlo Wahlstedt <carlowahlstedt@gmail.com>
2020-09-03 08:11:03 -0400

    rocket

--- homebrew-install.sh
+++ homebrew-install.sh
@@ -1,7 +1,11 @@
 
-#copy from where ever my script is
+# copy from where ever my script is
 brew install kubectx
 brew install k9s
 brew install eksctl
 brew install watch
+
+
+brew cask install rocket
+
 
